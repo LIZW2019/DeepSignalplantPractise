9eb04863fe5af8abc4402671051f5d0c2a98bcd4
LIZW2019 <zhuowenlee2018@126.com>
2022-07-04 14:37:03 +0800

    v2

---
+++ 08.split.sh
@@ -0,0 +1,3 @@
+python /scem/work/mowp/lizw/porec/TMP_bio_protocol/scripts/deepsignal-plant/scripts/split_freq_file_by_5mC_motif.py \
+--freqfile ./SINGLE_sample_data/fast5s.C.call_mods.freq.bed \
+--ref ./reference/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa
---
+++ bin_and_visualization/09.bin_weight_met_for_porecRep2.sh
@@ -0,0 +1,6 @@
+python met_level_bin.py \
+--region_bed reference/Tair10_genome.bed \
+--met_bed Rep2_fast5s.C.call_mods.CG.frequency.bed \
+--prefix Rep2_fast5s.C.call_mods.CG \
+--binsize 100 \
+--outdir ./
