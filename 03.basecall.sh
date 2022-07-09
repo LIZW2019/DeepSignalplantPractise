0c5f3a4e078309c210396a2cb477f4b97bbbda2e
LIZW2019 <zhuowenlee2018@126.com>
2022-07-09 18:07:45 +0800

    v4

--- 03.basecall.sh
+++ 03.basecall.sh
@@ -1,8 +1,7 @@
 guppy_basecaller \
 -i ./SINGLE_sample_data \
 -s ./SINGLE_sample_data/fastq \
--c dna_r9.4.1_450bps_hac.cfg \
+-c dna_r9.4.1_450bps_hac_prom.cfg \
 --recursive \
 --disable_pings \
 --qscore_filtering \
---device "cuda:all:100%"
