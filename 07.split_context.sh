08213fae91181fa5d71442a0abc30760208f408e
LIZW2019 <zhuowenlee2018@126.com>
2022-07-10 00:14:25 +0800

    2.0

---
+++ 07.split_context.sh
@@ -0,0 +1,3 @@
+python $PATHofDeepSignalPlant/scripts/split_freq_file_by_5mC_motif.py \
+--freqfile ./SINGLE_sample_data/fast5s.C.call_mods.freq.bed \
+--ref ./reference/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa
