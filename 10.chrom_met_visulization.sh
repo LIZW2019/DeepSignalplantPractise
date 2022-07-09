8efc5c8da95a404f286bb3e7d76aced992df7208
LIZW2019 <54759510+LIZW2019@users.noreply.github.com>
2022-07-10 00:16:41 +0800

    Delete 10.chrom_met_visulization.sh

--- 10.chrom_met_visulization.sh
+++
@@ -1,6 +0,0 @@
-python python_scripts/chrom_met_visulization.py \
---cg_bedg Rep2_fast5s.C.call_mods.CG_binsize100000.bedgraph \
---chg_bedg Rep2_fast5s.C.call_mods.CHG_binsize100000.bedgraph \
---chh_bedg Rep2_fast5s.C.call_mods.CHH_binsize100000.bedgraph \
---region_bed reference/Tair10_genome.bed \
---chrom 4 --outdir .
