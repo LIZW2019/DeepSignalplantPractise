d3f4f15fd7b6acdd987d1b4207dfbcdeea1400d9
LIZW2019 <54759510+LIZW2019@users.noreply.github.com>
2022-07-19 14:04:22 +0800

    Delete 02.basecall.sh

--- 02.basecall.sh
+++
@@ -1,9 +0,0 @@
-#02.basecall.sh
-guppy_basecaller \
--i ./SINGLE_sample_data \
--s ./SINGLE_sample_data/fastq \
--c dna_r9.4.1_450bps_hac_prom.cfg \
---recursive \
---disable_pings \
---qscore_filtering \
---device "cuda:all:100%"
