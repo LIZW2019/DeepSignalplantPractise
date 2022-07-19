6efe61ac967630f089cf450947f5a36586289e6b
LIZW2019 <54759510+LIZW2019@users.noreply.github.com>
2022-07-19 14:04:52 +0800

    Delete 06.deepplant-met-freq.sh

--- 06.deepplant-met-freq.sh
+++
@@ -1,8 +0,0 @@
-#06.deepplant-met-freq.sh
-#environment setting, replace $CondaEnv/deepsignalpenv with your actual path
-export PATH=$CondaEnv/deepsignalpenv/bin:$PATH
-#calculate frequency
-deepsignal_plant call_freq \
---input_path ./SINGLE_sample_data/fast5s.C.call_mods.tsv \
---result_file ./SINGLE_sample_data/fast5s.C.call_mods.freq.bed \
---sort --bed
