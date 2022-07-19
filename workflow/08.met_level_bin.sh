4136409c358181c88a51e073ae6346aba669c56c
LIZW2019 <zhuowenlee2018@126.com>
2022-07-19 14:21:59 +0800

    bioprotocolv3

--- workflow/08.met_level_bin.sh
+++ workflow/08.met_level_bin.sh
@@ -1,6 +1,6 @@
 #08.met_level_bin.sh
 python ../lib/python_scripts/met_level_bin.py \
---region_bed reference/Tair10_genome.bed \
+--region_bed ../input/reference/Tair10_genome.bed \
 --met_bed ../input/Step8_Input/porec_rep2/forstep08/Rep2_fast5s.C.call_mods.CG.frequency.bed \
 --prefix Rep2_fast5s.C.call_mods.CG \
 --binsize 100000 \
